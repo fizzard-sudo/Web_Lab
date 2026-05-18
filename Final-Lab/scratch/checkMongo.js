afd0fa7887e68ecf05570c2d2390e8bab2626cea
fizzard-sudo <P24-BCS-041@CUILAHORE.EDU.PK>
2026-05-18 08:14:24 +0500

    final lab exam

--- Final-Lab/scratch/checkMongo.js
+++
@@ -1,3 +0,0 @@
-const MongoStore = require('connect-mongo');
-console.log('MongoStore.MongoStore.create:', MongoStore.MongoStore ? MongoStore.MongoStore.create : 'No MongoStore.MongoStore');
-console.log('MongoStore.default.create:', MongoStore.default ? MongoStore.default.create : 'No MongoStore.default');
